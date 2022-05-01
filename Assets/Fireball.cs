3da0c49bf078b5257b6cf00732df3d6069d50bba
travisbird0 <tboydlink@gmail.com>
2022-05-01 13:53:03 -0500

    Fixed horde navmesh

--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    [SerializeField] private GameObject fireExplosion;
+    [SerializeField] private Collider ballHitbox;
+
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("Hit trigger: " + other.gameObject.name);
+        KABOOM();
     }
 
     public void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Hit collider: " + collision.gameObject.name);
+        KABOOM();
+    }
+
+    public void KABOOM()
+    {
+        ballHitbox.enabled = false;
+
+        GameObject boomObj = Instantiate(fireExplosion);
+        boomObj.transform.position = transform.position;
+
+        boomObj.transform.parent = null;
+        Destroy(gameObject);
     }
 }
